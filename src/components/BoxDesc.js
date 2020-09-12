feb44baa75a42ceecb8b118d176eb67fd0502acf
Christopher Flynn <cmf180003@utdallas.edu>
2020-09-11 22:22:01 -0500

    Added card header and footer (footer is optional and can be removed)

--- src/components/BoxDesc.js
+++ src/components/BoxDesc.js
@@ -2,40 +2,48 @@ import React from "react";
 import { Card, Button, CardDeck } from "react-bootstrap";
 
 function BoxDesc() {
-
   return (
-    <CardDeck className="mx-auto mt-2">
-        <Card style={{ width: "18rem" }} border="info">
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
+    <Card className="mx-auto mt-2" border="light">
+      <Card.Header className="text-center" as="h1">
+        Explore and learn below!
+      </Card.Header>
+      <CardDeck>
+        <Card style={{ width: "18rem" }} border="light">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
             <Card.Title>Scholarships</Card.Title>
             <Card.Text>
-            Want to learn more about how to make college affordable? Click below!
+              Want to learn more about how to make college affordable? Click
+              below!
             </Card.Text>
             <Button variant="primary">Learn More!</Button>
-        </Card.Body>
+          </Card.Body>
         </Card>
-        <Card style={{ width: "18rem" }} border="success">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
+        <Card style={{ width: "18rem" }} border="light">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
             <Card.Title>Colleges</Card.Title>
             <Card.Text>
-                Know you want to go to college, but unsure about your dream school? Click below!
+              Know you want to go to college, but unsure about your dream
+              school? Click below!
             </Card.Text>
             <Button variant="primary">Learn More!</Button>
-            </Card.Body>
+          </Card.Body>
         </Card>
-        <Card style={{ width: "18rem" }} border="secondary">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Careers</Card.Title>
-                <Card.Text>
-                Unsure about what you want to do later in life? That's alright! Click below to learn more.
-                </Card.Text>
-                <Button variant="primary">Learn More!</Button>
-            </Card.Body>
-            </Card>
+        <Card style={{ width: "18rem" }} border="light">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Careers</Card.Title>
+            <Card.Text>
+              Unsure about what you want to do later in life? That's alright!
+              Click below to learn more.
+            </Card.Text>
+            <Button variant="primary">Learn More!</Button>
+          </Card.Body>
+        </Card>
       </CardDeck>
+      <Card.Footer></Card.Footer>
+    </Card>
   );
 }
 
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,53 +1,40 @@
-import React, {useState} from 'react'
-import {Carousel} from 'react-bootstrap'
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
 
 export default function ControlledCarousel() {
-    const [index, setIndex] = useState(0);
-  
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
-  
-    return (
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-        <img
-            className="d-block w-100"
-            src="img/0.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="img/1.jpg"
-            alt="Second slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="img/2.jpg"
-            alt="Third slide"
-          />
-  
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
-  }
+  const [index, setIndex] = useState(0);
 
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className="d-block w-100" src="img/0.jpg" alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="img/1.jpg" alt="Second slide" />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src="img/2.jpg" alt="Third slide" />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
