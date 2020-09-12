c29e63a8b87aa6491ea0c4376a186a74fad44e5b
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-09-12 08:22:55 -0500

    Remove chatbot link from navbar for real this time pt2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,6 @@ export default function App() {
           <Nav.Link href="/scholarships">Scholarships</Nav.Link>
           <Nav.Link href="/colleges">Colleges</Nav.Link>
           <Nav.Link href="/careers">Careers</Nav.Link>
-          <Nav.Link href="/chatbot">Chatbot</Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
