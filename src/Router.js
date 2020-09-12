0a9dbb1037632ab93ff41b83e0dc3425bded3202
Jessela26 <jelbaniqued26@gmail.com>
2020-09-12 05:41:13 -0400

    added details page

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './routes/index';
 import ChatBot from './routes/chatbot/index';
 import Careers from './routes/careers/index'
+import JobDetails from './routes/jobdetails/index';
+
 function App() {
   return (
       <BrowserRouter>
@@ -11,6 +13,7 @@ function App() {
               <Route path="/example" component={LandingPage} />
               <Route path="/chatbot" component={ChatBot} />
               <Route path="/careers" component={Careers} />
+              <Route path="/jobdetails" component={JobDetails}/>
             </Switch>
       </BrowserRouter>
   );
