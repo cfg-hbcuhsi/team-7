82aa11404c81ef915f0841df7e1a372a7b5cb1c4
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-09-12 01:00:44 -0500

    Updates to the landing page

--- src/components/BoxDesc.js
+++ src/components/BoxDesc.js
@@ -3,7 +3,7 @@ import { Card, Button, CardDeck } from "react-bootstrap";
 import styled from "styled-components";
 
 function BoxDesc() {
-    
+
   return (
     <Card className="mx-auto mt-2" border="light">
     <Card.Header className="text-center" as="h1">
@@ -12,7 +12,7 @@ function BoxDesc() {
     <CardDeck className="mx-auto mt-2">
       <Card style={{ width: "18rem" }} border="info">
         <Card.Body>
-            <i class="fas fa-money-bill-wave fa-4x d-block text-center"></i>
+            <i className="fas fa-money-bill-wave fa-4x d-block text-center"></i>
           <Card.Title className="text-center">Scholarships</Card.Title>
           <Card.Text className="text-center">
             Want to learn more about how to make college affordable? Click
@@ -29,7 +29,7 @@ function BoxDesc() {
       </Card>
       <Card style={{ width: "18rem" }} border="success">
         <Card.Body>
-        <i class="fas fa-graduation-cap fa-4x d-block text-center"></i>
+        <i className="fas fa-graduation-cap fa-4x d-block text-center"></i>
           <Card.Title className="text-center">Colleges</Card.Title>
           <Card.Text className="text-center">
             Know you want to go to college, but unsure about your dream school?
@@ -46,7 +46,7 @@ function BoxDesc() {
       </Card>
       <Card style={{ width: "18rem" }} border="secondary">
         <Card.Body>
-        <i class="fas fa-briefcase fa-4x d-block text-center"></i>
+        <i className="fas fa-briefcase fa-4x d-block text-center"></i>
           <Card.Title className="text-center">Careers</Card.Title>
           <Card.Text className="text-center">
             Unsure about what you want to do later in life? That's alright!
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Carousel from "../components/Carousel";
 import BoxDesc from "../components/BoxDesc";
-import { Row, Container, Col, Card } from "react-bootstrap";
+import { Row, Container, Col, Card, Button } from "react-bootstrap";
 
 export default function IndexPage() {
   return (
@@ -11,10 +11,71 @@ export default function IndexPage() {
       <Container>
         <Row>
           <Carousel />
-          <BoxDesc />
+        </Row>
+        <BoxDesc />
+        <Row>
+          <Col xs={12} md={6} className="my-auto text-center">
+            <h4>The chart is based upon the <a href="https://www.bls.gov/careeroutlook/2018/data-on-display/education-pays.htm" >'U.S. Bureau of Labor Stastics'</a> research numbers which showcases the median weekly earnings in 2017 for those with the highest levels of educational attainment crossed by unemployment rates for that educational level. The chart is a true showcase of the statement more you learn, the more you earn.</h4>
+          </Col>
+          <Col className="mx-auto" xs={12} md={6}>
+            <img
+              src="/img/graph.jpg"
+              alt="Eat Learn Play"
+              className="img-fluid mt-3"
+              style={{ width: "100%" }}
+            />
+          </Col>
         </Row>
         <Row>
+          <img
+            src="/img/elp.jpg"
+            alt="Eat Learn Play"
+            className="img-fluid mt-5"
+            style={{ width: "100%" }}
+          />
         </Row>
+        <Row>
+          <h1 className="mx-auto mt-3" style={{ fontFamily: "Arial" }}>
+            Their Mission:
+          </h1>
+        </Row>
+        <Row>
+          <h4 style={{ fontFamily: "Arial" }} className="text-center">
+            Stephen and Ayesha Curry’s Eat. Learn. Play. Foundation is committed
+            to unlocking the amazing potential of every child by fighting to end
+            childhood hunger, ensuring students have access to a quality
+            education, and providing safe places for all children to play and be
+            active.
+          </h4>
+        </Row>
+
+        {/* <Card style={{ width: "50rem" }}>
+          <Row>
+            <Col m={3}>
+              <Card.Img
+                variant=""
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRg4Va7YMqM9loi49RwZubmPd_YluiwiatPeg&usqp=CAU%22/%3E"
+              />
+            </Col>
+            <Col m={3}>
+              <Card.Body>
+                <Card.Title>
+                  Software Developer - Median Salary: $103,620
+                </Card.Title>
+                <Card.Text>
+                  Software developers invent the technologies we sometimes take
+                  for granted. For instance, that app that rings, sings or
+                  buzzes you out of a deep sleep every morning? A software
+                  developer helped design that. And when you roll into the
+                  office and turn on your computer, clicking and scrolling
+                  through social media, music and your personal calendar –
+                  software developers had a big hand in shaping those, too.
+                </Card.Text>
+                <Button variant="primary">See More</Button>
+              </Card.Body>
+            </Col>
+          </Row>
+        </Card> */}
       </Container>
     </>
   );
