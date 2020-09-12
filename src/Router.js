fd954415b144bf1012b3ca3669c4c5c8e9783890
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-09-12 08:13:08 -0500

    Remove chatbot link from navbar

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './routes/index';
-import ChatBot from './routes/chatbot/index';
 import Careers from './routes/careers/index'
 import JobDetails from './routes/careers/id';
 
@@ -11,7 +10,6 @@ function App() {
             <Switch>
               <Route path="/" component={LandingPage} exact />
               <Route path="/example" component={LandingPage} />
-              <Route path="/chatbot" component={ChatBot} />
               <Route path="/careers/:id" component={JobDetails}/>
               <Route path="/careers" component={Careers} />
             </Switch>
